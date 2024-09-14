316c13b720871a991c9a354994c9a8e002566e78
hoppv <phamhopxd1@gmail.com>
2024-09-14 08:39:17 +0700

    auth routes set up

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
 
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
