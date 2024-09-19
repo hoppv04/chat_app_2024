06ddace1c61eed41044d2ad9631d3ac6b7f08e08
hoppv <phamhopxd1@gmail.com>
2024-09-19 10:09:19 +0700

    get conversations

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,22 +1,31 @@
+import { useEffect } from "react";
+import useConversation from "../../zustand/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import NoChatSelected from "./NoChatSelected";
 
 const MessageContainer = () => {
-  const noChatSelected = true;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  useEffect(() => {
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
+
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {noChatSelected ? (
-        <NoChatSelected />
-      ) : (
+      {selectedConversation ? (
         <>
           <div className="bg-slate-500 px-4 py-2 mb-2">
             <span className="label-text">To:</span>{" "}
-            <span className="text-gray-900 font-bold">John doe</span>
+            <span className="text-gray-900 font-bold">
+              {selectedConversation?.fullName}
+            </span>
           </div>
           <Messages />
           <MessageInput />
         </>
+      ) : (
+        <NoChatSelected />
       )}
     </div>
   );
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,24 @@
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+
   return (
     <div className="py-2 flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
+      {conversations?.length > 0 &&
+        conversations.map((conversationItem, index) => (
+          <Conversation
+            key={conversationItem?._id}
+            conversation={conversationItem}
+            emoji={getRandomEmoji()}
+            lastIndex={index === conversations.length - 1}
+          />
+        ))}
     </div>
   );
 };
